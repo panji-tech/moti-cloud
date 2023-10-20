2a5234254f90bffe9af750aee0c7bde8a6f32515
itguang <itguang@qq.com>
2023-10-20 21:17:17 +0800

    itsource 定制v2

--- src/main/java/com/moti/utils/FtpUtil.java
+++ src/main/java/com/moti/utils/FtpUtil.java
@@ -1,12 +1,13 @@
 package com.moti.utils;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName: FtpUtil
@@ -15,12 +16,11 @@ import java.io.*;
  * @date 2020/2/5 10:41
  * @Version: 1.0
  **/
-@Slf4j
 public class FtpUtil {
     /**
      * FTP服务器hostname
      */
-    private static String HOST = "localhost";
+    private static String HOST = "127.0.0.1";
     /**
      * FTP服务器端口
      */
@@ -36,85 +36,69 @@ public class FtpUtil {
     /**
      * FTP服务器基础目录
      */
-    private static String BASEPATH = "/wangpan";
+    private static String BASEPATH = "";
     /**
      * FTP客户端
      */
     private static FTPClient ftp;
 
     /**
-     * @return boolean
      * @Description 初始化FTP客户端
      * @Author xw
      * @Date 12:34 2020/2/5
      * @Param []
+     * @return boolean
      **/
-    public static boolean initFtpClient() {
-
+    public static boolean initFtpClient(){
+        ftp = new FTPClient();
         int reply;
         try {
-            if (ftp == null) {
-                ftp = createFtp();
-            }
+            // 连接FTP服务器
+            ftp.connect(HOST, PORT);
+            //登录, 如果采用默认端口，可以使用ftp.connect(host)的方式直接连接FTP服务器
+            ftp.login(USERNAME, PASSWORD);
+            ftp.setBufferSize(10240);
+            //设置传输超时时间为60秒
+            ftp.setDataTimeout(600000);
+            //连接超时为60秒
+            ftp.setConnectTimeout(600000);
+            //FTP以二进制形式传输
+            ftp.setFileType(FTP.BINARY_FILE_TYPE);
             reply = ftp.getReplyCode();
             if (!FTPReply.isPositiveCompletion(reply)) {
                 ftp.disconnect();
                 return false;
             }
-            log.info("FTP 服务器连接成功！");
-        } catch (Exception e) {
-            log.info("FTP 服务器连接异常：{}", e.getCause(), e);
-            throw new RuntimeException("FTP 服务连接异常:" + e.getCause());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return true;
-    }
-
-    private static FTPClient createFtp() throws IOException {
-        FTPClient newFtp = new FTPClient();
-        // 连接FTP服务器
-        newFtp.connect(HOST, PORT);
-        //登录, 如果采用默认端口，可以使用ftp.connect(host)的方式直接连接FTP服务器
-        newFtp.login(USERNAME, PASSWORD);
-        newFtp.setBufferSize(10240);
-        //设置传输超时时间为60秒
-        newFtp.setDataTimeout(600000);
-        //连接超时为60秒
-        newFtp.setConnectTimeout(600000);
-        //FTP以二进制形式传输
-        newFtp.setFileType(FTP.BINARY_FILE_TYPE);
-        return newFtp;
-    }
-
-    public static void main(String[] args) {
-        FtpUtil.initFtpClient();
     }
 
 
     /**
      * Description: 向FTP服务器上传文件
-     *
      * @param filePath FTP服务器文件存放路径。例如分日期存放：/2015/01/01。文件的路径为basePath+filePath
      * @param filename 上传到FTP服务器上的文件名
-     * @param input    本地要上传的文件的 输入流
+     * @param input 本地要上传的文件的 输入流
      * @return 成功返回true，否则返回false
      */
     public static boolean uploadFile(String filePath, String filename, InputStream input) {
         boolean result = false;
         try {
-            filePath = new String(filePath.getBytes("GBK"), "iso-8859-1");
-            filename = new String(filename.getBytes("GBK"), "iso-8859-1");
-            if (!initFtpClient()) {
+            filePath = new String(filePath.getBytes("GBK"),"iso-8859-1");
+            filename = new String(filename.getBytes("GBK"),"iso-8859-1");
+            if (!initFtpClient()){
                 return result;
-            }
-            ;
+            };
             //切换到上传目录
             ftp.enterLocalPassiveMode();
-            if (!ftp.changeWorkingDirectory(BASEPATH + filePath)) {
+            if (!ftp.changeWorkingDirectory(BASEPATH+filePath)) {
                 //如果目录不存在创建目录
                 String[] dirs = filePath.split("/");
                 String tempPath = BASEPATH;
                 for (String dir : dirs) {
-                    if (null == dir || "".equals(dir)) {
+                    if (null == dir || "".equals(dir)){
                         continue;
                     }
                     tempPath += "/" + dir;
@@ -137,7 +121,8 @@ public class FtpUtil {
             input.close();
             ftp.logout();
             result = true;
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         } finally {
             if (ftp.isConnected()) {
@@ -152,21 +137,19 @@ public class FtpUtil {
 
     /**
      * Description: 从FTP服务器下载文件
-     *
      * @param remotePath FTP服务器上的相对路径
-     * @param fileName   要下载的文件名
+     * @param fileName 要下载的文件名
      * @return
      */
-    public static boolean downloadFile(String remotePath, String fileName, String localPath) {
+    public static boolean downloadFile( String remotePath,String fileName,String localPath) {
         boolean result = false;
 
         try {
-            remotePath = new String(remotePath.getBytes("GBK"), "iso-8859-1");
-            fileName = new String(fileName.getBytes("GBK"), "iso-8859-1");
-            if (!initFtpClient()) {
+            remotePath = new String(remotePath.getBytes("GBK"),"iso-8859-1");
+            fileName = new String(fileName.getBytes("GBK"),"iso-8859-1");
+            if (!initFtpClient()){
                 return result;
-            }
-            ;
+            };
             // 转移到FTP服务器目录
             ftp.changeWorkingDirectory(remotePath);
             ftp.enterLocalPassiveMode();
@@ -175,7 +158,7 @@ public class FtpUtil {
                 if (ff.getName().equals(fileName)) {
                     ftp.enterLocalPassiveMode();
                     FileOutputStream outputStream = new FileOutputStream(new File(localPath));
-                    ftp.retrieveFile(remotePath + "/" + fileName, outputStream);
+                    ftp.retrieveFile(remotePath+"/"+fileName,outputStream);
 
                     result = true;
                     outputStream.close();
@@ -196,21 +179,20 @@ public class FtpUtil {
     }
 
     /**
-     * @return boolean
      * @Description 从ftp服务器下载文件到指定输出流
      * @Author xw
      * @Date 22:30 2020/3/5
      * @Param [remotePath, fileName, outputStream]
+     * @return boolean
      **/
     public static boolean downloadFile(String remotePath, String fileName, OutputStream outputStream) {
         boolean result = false;
         try {
-            remotePath = new String(remotePath.getBytes("GBK"), "iso-8859-1");
-            fileName = new String(fileName.getBytes("GBK"), "iso-8859-1");
-            if (!initFtpClient()) {
+            remotePath = new String(remotePath.getBytes("GBK"),"iso-8859-1");
+            fileName = new String(fileName.getBytes("GBK"),"iso-8859-1");
+            if (!initFtpClient()){
                 return result;
-            }
-            ;
+            };
             // 转移到FTP服务器目录
             ftp.changeWorkingDirectory(remotePath);
             ftp.enterLocalPassiveMode();
@@ -218,7 +200,7 @@ public class FtpUtil {
             for (FTPFile ff : fs) {
                 if (ff.getName().equals(fileName)) {
                     ftp.enterLocalPassiveMode();
-                    ftp.retrieveFile(remotePath + "/" + fileName, outputStream);
+                    ftp.retrieveFile(remotePath+"/"+fileName,outputStream);
                     result = true;
                 }
             }
@@ -237,31 +219,30 @@ public class FtpUtil {
     }
 
     /**
-     * @return void
      * @Description 删除文件
      * @Author xw
      * @Date 11:38 2020/2/6
      * @Param [remotePath, fileName]
+     * @return void
      **/
-    public static boolean deleteFile(String remotePath, String fileName) {
+    public static boolean deleteFile( String remotePath,String fileName){
         boolean flag = false;
         try {
-            remotePath = new String(remotePath.getBytes("GBK"), "iso-8859-1");
-            fileName = new String(fileName.getBytes("GBK"), "iso-8859-1");
-            if (!initFtpClient()) {
+            remotePath = new String(remotePath.getBytes("GBK"),"iso-8859-1");
+            fileName = new String(fileName.getBytes("GBK"),"iso-8859-1");
+            if (!initFtpClient()){
                 return flag;
-            }
-            ;
+            };
             // 转移到FTP服务器目录
             ftp.changeWorkingDirectory(remotePath);
             ftp.enterLocalPassiveMode();
             FTPFile[] fs = ftp.listFiles();
             for (FTPFile ff : fs) {
-                if ("".equals(fileName)) {
+                if ("".equals(fileName)){
                     return flag;
                 }
-                if (ff.getName().equals(fileName)) {
-                    String filePath = remotePath + "/" + fileName;
+                if (ff.getName().equals(fileName)){
+                    String filePath = remotePath + "/" +fileName;
                     ftp.deleteFile(filePath);
                     flag = true;
                 }
@@ -281,25 +262,24 @@ public class FtpUtil {
     }
 
     /**
-     * @return void
      * @Description 删除文件夹
      * @Author xw
      * @Date 11:38 2020/2/6
      * @Param [remotePath, fileName]
+     * @return void
      **/
-    public static boolean deleteFolder(String remotePath) {
+    public static boolean deleteFolder( String remotePath){
         boolean flag = false;
         try {
-            remotePath = new String(remotePath.getBytes("GBK"), "iso-8859-1");
-            if (!initFtpClient()) {
+            remotePath = new String(remotePath.getBytes("GBK"),"iso-8859-1");
+            if (!initFtpClient()){
                 return flag;
-            }
-            ;
+            };
             // 转移到FTP服务器目录
             ftp.changeWorkingDirectory(remotePath);
             ftp.enterLocalPassiveMode();
             FTPFile[] fs = ftp.listFiles();
-            if (fs.length == 0) {
+            if (fs.length==0){
                 ftp.removeDirectory(remotePath);
                 flag = true;
             }
@@ -318,23 +298,22 @@ public class FtpUtil {
     }
 
     /**
-     * @return boolean
      * @Description 修改文件名称或者文件夹名
      * @Author xw
      * @Date 21:18 2020/2/11
      * @Param [oldAllName, newAllName]
+     * @return boolean
      **/
-    public static boolean reNameFile(String oldAllName, String newAllName) {
+    public static boolean reNameFile( String oldAllName,String newAllName){
         boolean flag = false;
         try {
-            oldAllName = new String(oldAllName.getBytes("GBK"), "iso-8859-1");
-            newAllName = new String(newAllName.getBytes("GBK"), "iso-8859-1");
-            if (!initFtpClient()) {
+            oldAllName = new String(oldAllName.getBytes("GBK"),"iso-8859-1");
+            newAllName = new String(newAllName.getBytes("GBK"),"iso-8859-1");
+            if (!initFtpClient()){
                 return flag;
-            }
-            ;
+            };
             ftp.enterLocalPassiveMode();
-            ftp.rename(oldAllName, newAllName);
+            ftp.rename(oldAllName,newAllName);
             flag = true;
             ftp.logout();
         } catch (IOException e) {
